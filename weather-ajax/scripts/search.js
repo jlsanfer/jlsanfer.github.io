511a35413040edfe3fa0ed01cf8b08ed4801c114
Leo Sanchez <jleonardo.sanchez@gmail.com>
2017-07-05 09:05:15 -0600

    qq

--- weather-ajax/scripts/search.js
+++ weather-ajax/scripts/search.js
@@ -19,26 +19,33 @@ $('#query').keyup(function() {
   }); // end getJSON
 }); // end onkeyup
 
+function twoDigits(number){
+  return (number < 10 ? '0' : '') + number
+}
+
 function getData(input) {
   event.preventDefault();
   $("#searchResults").html('');
   // Get the data from the wunderground API
+  var todayDate = new Date();
+  var YYYYDDMM = todayDate.getUTCFullYear() + "" + twoDigits(todayDate.getUTCMonth()) + "" + twoDigits(todayDate.getUTCDate());
+  window.alert(YYYYDDMM);
   $.ajax({
-    url: "//api.wunderground.com/api/257fcd7045f9ce1c/geolookup/conditions"
-    + input + ".json"
+    url: "//api.wunderground.com/api/257fcd7045f9ce1c/almanac/history_" + YYYYDDMM + "geolookup/conditions/q/location/" + input + ".json"
     , dataType: "jsonp"
     , success: function (data) {
       console.log(data);
-      var location = data.location.city + ', ' + data.location.state;
+      var location = data.current_observation.display_location.full;
       var temp_f = data.current_observation.temp_f;
-      console.log('Location is: ' + location);
-      console.log('Temp is: ' + temp_f);
-      $("#cityDisplay").text(location);
+      console.log('Location ' + location);
+      console.log('Current Temp ' + temp_f);
       $("title").html(location + " | Weather Center");
-      $("#currentTemp").html(Math.round(temp_f) + '°');
-      $("#currentTemps").html(data.current_observation.temperature_string);
+      $("#cityDisplay").text(location);
+      $("#currentTemp").html(Math.round(temp_f) + ' °F');
       $("#summary").text(data.current_observation.weather);
-      $("#humidity").html(data.current_observation.relative_humidity);
+      $("#humidity").html('Humidity: ' + data.current_observation.relative_humidity);
+      $("#minTemp").html('Min Temp: ' + data.almanac.temp_low.normal.F + ' °F');
+      $("#maxTemp").html('Max Temp: ' + data.almanac.temp_high.normal.F + ' °F');
 
       $("#cover").fadeOut(250);
     }
